6d621344c85b8aa230e8582d3b12525f3bb0c97c
Jobin <jobin.jia@beefinx.com>
2020-03-07 23:17:17 +0800

    add themes

---
+++ hooks/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd ..
+pwd
+rm -rf public.zip
+zip -q -r -o public.zip public
+scp -i '/Users/jiabinbin/.ssh/root' public.zip root@150.109.111.71:../workspace/blog/
+ssh -i '/Users/jiabinbin/.ssh/root' root@150.109.111.71 'cd /workspace/blog/ ; ./deploy.sh'
