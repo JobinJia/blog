2fb767c65cbbe7e0caedcab236469f575dec67c3
Jobin <jobin.jia@beefinx.com>
2020-03-11 14:13:58 +0800

    author: update blog

--- hooks/deploy.sh
+++ hooks/deploy.sh
@@ -7,3 +7,8 @@ zip -q -r -o public.zip public
 scp -i '/Users/jiabinbin/.ssh/root' public.zip root@150.109.111.71:../workspace/blog/
 ssh -i '/Users/jiabinbin/.ssh/root' root@150.109.111.71 'cd /workspace/blog/ ; ./deploy.sh'
 rm -rf public.zip
+git status
+git add .
+git commit -m 'author: update blog'
+git push origin master
+echo 'task finished'
